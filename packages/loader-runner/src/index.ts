5cc0a422a19a6f47c5972a208bd57b59102cfb46
xieyuhang <13451213869@163.com>
2022-08-23 23:06:54 +0800

    delete files

--- packages/loader-runner/src/index.ts
+++
@@ -1,3 +0,0 @@
-export function loaderRunner() {
-  return 'loader runnder'
-}
--- packages/my-webpack/src/index.ts
+++ packages/my-webpack/src/index.ts
@@ -1,4 +1,3 @@
-import { loaderRunner } from '@my-webpack/loader-runner'
 import { shared } from '@my-webpack/shared'
 
-export default shared() + loaderRunner()
+export default shared()
